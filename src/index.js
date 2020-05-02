cea2d2a7e4d783c385d240cd075eb95ba3e4ba5d
hugosanga <hugo.fsangaletti@gmail.com>
2020-05-02 16:15:01 -0300

    Https problema

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const secure = require('express-force-https')
+// const secure = require('express-force-https')
 
 const routes = require('./routes')
 
@@ -8,7 +8,7 @@ app = express()
 
 app.use(cors({ origin: 'https://rebalanceamento-frontend.herokuapp.com' }))
 app.use(express.json())
-app.use(secure)
+// app.use(secure)
 app.use(routes)
 
 app.listen(process.env.PORT || 3333)
