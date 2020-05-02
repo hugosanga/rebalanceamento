252fc74e788c71673331362c47ad74aca93e58a4
hugosanga <hugo.fsangaletti@gmail.com>
2020-05-02 15:59:43 -0300

    https only

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const secure = require('express-force-https')
 
 const routes = require('./routes')
 
@@ -7,6 +8,7 @@ app = express()
 
 app.use(cors({ origin: 'https://rebalanceamento-frontend.herokuapp.com' }))
 app.use(express.json())
+app.use(secure)
 app.use(routes)
 
 app.listen(process.env.PORT || 3333)
