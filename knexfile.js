b76a0a6efe102c10a632f1f73daf6d745e97fae4
hugosanga <hugo.fsangaletti@gmail.com>
2020-05-02 16:02:23 -0300

    Ajuste knexfile

--- knexfile.js
+++ knexfile.js
@@ -13,24 +13,8 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/src/database/migrations'
